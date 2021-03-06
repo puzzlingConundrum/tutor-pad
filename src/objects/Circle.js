2ed80792d096d93800e81347e459c0f95bec7a2d
poohcom1 <pooh677@gmail.com>
2021-03-07 00:22:05 +0700

    Fixed reverse circle size not drawing properly

--- src/objects/Circle.js
+++ src/objects/Circle.js
@@ -10,8 +10,21 @@ export class Circle extends Shape {
     }
 
     preview(context, initX, initY, finalX, finalY) {
+  
         let radiusX = Math.abs(finalX-initX);
         let radiusY = Math.abs(finalY-initY);
+
+        // Check if initial pos are greater than the final pos, if so swap them
+        if (initX > finalX) {
+            let temp = initX;
+            initX = finalX;
+            finalX = temp;
+        }
+        if (initY > finalY) {
+            let temp = initY;
+            initY = finalY;
+            finalY = temp;
+        }
         context.beginPath();
         context.ellipse(initX+0.5*radiusX, initY+0.5*radiusY, 0.5*radiusX, 0.5*radiusY, 0, 0, 2*Math.PI);
         context.closePath();
